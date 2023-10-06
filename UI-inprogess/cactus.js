cb56f75bba68c3140d3883798d10bd0ae62921cf
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-10-06 16:36:19 +0800

    add cactus

--- UI-inprogess/cactus.js
+++ UI-inprogess/cactus.js
@@ -24,9 +24,22 @@ export function updateCactus(delta, speedScale) {
       cactus.remove();
     }
   });
+  document
+    .querySelectorAll("[data-flyingObstacle]")
+    .forEach((flyingObstacle) => {
+      incrementCustomProperty(
+        flyingObstacle,
+        "--left",
+        delta * speedScale * SPEED * -1
+      );
+      if (getCustomProperty(flyingObstacle, "--left") <= -100) {
+        flyingObstacle.remove();
+      }
+    });
 
   if (nextCactusTime <= 0) {
     createCactus();
+    createFlyingObstacle();
     nextCactusTime =
       randomNumberBetween(CACTUS_INTERVAL_MIN, CACTUS_INTERVAL_MAX) /
       speedScale;
@@ -43,18 +56,27 @@ export function getCactusRects() {
 const obstacleSprites = [
   "imgs/obstacle1.png",
   "imgs/obstacle2.png",
-  "imgs/obstacle3.png"
-]
+  "imgs/obstacle3.png",
+];
 
 function createCactus() {
   const cactus = document.createElement("img");
   cactus.dataset.cactus = true;
   cactus.src = obstacleSprites[Math.floor(Math.random() * 3)];
   cactus.classList.add("cactus");
-  setCustomProperty(cactus, "--left", 100);
+  setCustomProperty(cactus, "--left", 10);
   worldElem.append(cactus);
 }
 
 function randomNumberBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function createFlyingObstacle() {
+  const flyingObstacle = document.createElemet("img");
+  flyingObstacle.dataset.flyingObstacle = true;
+  flyingObstacle.src = obstacleSprites[Math.floor(Math.random() * 3)];
+  flyingObstacle.classList.add("flyingObstacle");
+  setCustomProperty(flyingObstacle, "--left", 100);
+  worldElem.append(flyingObstacle);
 }
